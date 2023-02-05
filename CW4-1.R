9e6118511cc852d27ad8cace553c3a0dda629347
TomaszKoder <t.urbaniak1990@gmail.com>
2023-02-05 21:52:58 +0100

    pare linijek wiecej

--- CW4-1.R
+++ CW4-1.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(vegan)
 
 #załadowanie danych
 data.set <- mtcars
@@ -7,10 +8,13 @@ data.set <- mtcars
 boxplot(mtcars)
 data.set <- data.set %>% scale(center = TRUE, scale = TRUE) 
 
+#hierarchiczna analiza skupień
 distances <- dist(data.set, method = 'euclidean')
-
 tree.average <- hclust(distances, method = 'average')
-
 plot(tree.average, hang = -1)
 lab.average <- cutree(tree.average, 5)
 rect.hclust(tree.average, k = 5, border = 2, cluster = lab.average)
+
+#ustalenie liczby clastrów
+?cascadeKM
+cars.cascade <- cascadeKM()
