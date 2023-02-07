3c7d20caefbf0dff96da9cf6b154d31824cd85dc
TomaszKoder <t.urbaniak1990@gmail.com>
2023-02-07 21:56:23 +0100

    K-means added

--- CW4-1.R
+++ CW4-1.R
@@ -1,5 +1,8 @@
 library(dplyr)
 library(vegan)
+library(cluster)
+
+require(ggplot2)
 
 #załadowanie danych
 data.set <- mtcars
@@ -15,6 +18,21 @@ plot(tree.average, hang = -1)
 lab.average <- cutree(tree.average, 5)
 rect.hclust(tree.average, k = 5, border = 2, cluster = lab.average)
 
-#ustalenie liczby clastrów
-?cascadeKM
-cars.cascade <- cascadeKM()
+#ustalenie liczby clastrów                
+cars.cascade <- cascadeKM(data.set, inf.gr = 2, sup.gr = 10, iter = 10, criterion = 'calinski')
+cars.cascade$results
+plot(cars.cascade)
+
+
+#nie-hierarchiczna analiza skupień (k-means)
+cars.kmeans <- kmeans(data.set, centers = 4, nstart = 100)
+cars.kmeans$cluster
+cars.kmeans$size
+
+sil.index <- silhouette(cars.kmeans$cluster,
+                        dist = dist(data.set, 
+                                    method = 'euclidean'))
+sil.index
+summary(sil.index)
+mean(sil.index[, 3])
+plot(sil.index)
